83792d0345d9604a505c436d1bac4382e16d3f3b
colinaardsma <colin.aardsma@gmail.com>
2016-11-03 14:48:10 -0500

    fully working hello world/name

--- hello-spring/src/main/java/org/launchcode/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/controllers/HelloController.java
@@ -1,5 +1,27 @@
 package org.launchcode.controllers;
 
-public class HelloController {
+import javax.servlet.http.HttpServletRequest; // gets data from http requests (ie. headers, queries)
 
+import org.launchcode.models.HelloMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // tell Spring this class as a controller (similar to component)
+public class HelloController {
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET) // tell Spring to use this method for all get requests to "/hello"
+	@ResponseBody // allows returning plain string (instead of a template)
+	public String hello(HttpServletRequest request) {
+		// get name parameter from request
+		String name = request.getParameter("name");
+		
+		if (name == null) {
+			name = "World";
+		}
+		
+		return "<h1>" + HelloMessage.getMessage(name) + "</h1>";
+	}
+	
 }
